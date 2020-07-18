5d7339d9b629a34c41c092d09cfe654cf9639530
Dipak <deepakgogoi18@gmail.com>
2020-07-19 01:04:07 +0530

    validation done only in Login

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,10 @@ import 'package:flutter/material.dart';
 import 'signup.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(Signup());
 }
 
-class MyApp extends StatelessWidget {
+class Signup extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,17 +25,23 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final _formKey = GlobalKey<FormState>();
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  String _email;
+  String _password;
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       resizeToAvoidBottomPadding: false,
-      body: Column(
+      body: SingleChildScrollView(
+        child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Container(
-            padding: EdgeInsets.only(left: 10.0),
-            child: Stack(
-              children: <Widget>[
+              padding: EdgeInsets.only(left: 10.0),
+              child: Stack(children: <Widget>[
                 Container(
                   padding: EdgeInsets.fromLTRB(15.0, 50.0, 0, 0),
                   child: Text(
@@ -68,135 +74,164 @@ class _MyHomePageState extends State<MyHomePage> {
                       color: Colors.blueAccent[400],
                     ),
                   ),
-                )
-              ],
-            ),
-          ),
+                ),
+              ])),
           Container(
-            padding: EdgeInsets.only(top: 25.0, left: 30.0, right: 30.0),
-            child: Column(
-              children: <Widget>[
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    labelStyle: TextStyle(
-                      fontFamily: '',
-                      fontWeight: FontWeight.bold,
-                      color: Colors.grey,
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey)),
-                  ),
+            padding: const EdgeInsets.all(20.0),
+            child: formSetup(context),
+          )
+        ],
+      ),)
+    );
+  }
+
+  Widget formSetup(BuildContext context) {
+    return new Form(
+        key: _formKey,
+        child: new Column(
+          children: <Widget>[
+            new TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Email',
+                labelStyle: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey,
                 ),
-                SizedBox(height: 20.0),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    labelStyle: TextStyle(
-                      fontFamily: '',
-                      fontWeight: FontWeight.bold,
-                      color: Colors.grey,
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey)),
-                  ),
-                  obscureText: true,
+                focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey)),
+              ),
+              keyboardType: TextInputType.emailAddress,
+              validator: (val) {
+                if (val.length == 0)
+                  return "Please enter email";
+                else if (!val.contains("@"))
+                  return "Please enter valid email";
+                else
+                  return null;
+              },
+              onSaved: (val) => _email = val,
+            ),
+            new TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Password',
+                labelStyle: TextStyle(
+                  fontFamily: '',
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey,
                 ),
-                SizedBox(height: 20.0),
-                Container(
-                  alignment: Alignment(1.0, 0.0),
-                  // padding: EdgeInsets.only(top: 0, left: 0.0),
-                  child: InkWell(
+                focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey)),
+              ),
+              obscureText: true,
+              validator: (val) {
+                if (val.length == 0)
+                  return "Please enter password";
+                else if (val.length <= 5)
+                  return "Your password should be more then 6 char long";
+                else
+                  return null;
+              },
+              onSaved: (val) => _password = val,
+            ),
+            SizedBox(height: 20.0),
+            Container(
+              alignment: Alignment(1.0, 0.0),
+              // padding: EdgeInsets.only(top: 0, left: 0.0),
+              child: InkWell(
+                child: Text(
+                  'Forgot Password ?',
+                  style: TextStyle(
+                      color: Colors.blueAccent[400],
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            SizedBox(height: 40.0),
+            Container(
+              height: 40.0,
+              child: Material(
+                borderRadius: BorderRadius.circular(20.0),
+                shadowColor: Colors.blueAccent,
+                color: Colors.blueAccent[400],
+                elevation: 7.0,
+                child: GestureDetector(
+                  onTap: () {
+                    if (_formKey.currentState.validate()) {
+                      _formKey.currentState.save();
+                      _scaffoldKey.currentState.showSnackBar(new SnackBar(
+                        content: new Text(
+                            "Your email: $_email and Password: $_password"),
+                      ));
+                    }
+                  },
+                  child: Center(
                     child: Text(
-                      'Forgot Password ?',
+                      'LOGIN',
                       style: TextStyle(
-                          color: Colors.blueAccent[400],
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 40.0),
-                Container(
-                  height: 40.0,
-                  child: Material(
-                    borderRadius: BorderRadius.circular(20.0),
-                    shadowColor: Colors.blueAccent,
-                    color: Colors.blueAccent[400],
-                    elevation: 7.0,
-                    child: GestureDetector(
-                      onTap: () {},
-                      child: Center(
-                        child: Text(
-                          'LOGIN',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
                 ),
-                SizedBox(height: 20.0),
-                Container(
-                  height: 40.0,
-                  // color: Colors.transparent,
-                  child: Container(
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.black,
-                          style: BorderStyle.solid,
-                          width: 1.0,
+              ),
+            ),
+            SizedBox(height: 20.0),
+            Container(
+              height: 40.0,
+              // color: Colors.transparent,
+              child: Container(
+                decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.black,
+                      style: BorderStyle.solid,
+                      width: 1.0,
+                    ),
+                    color: Colors.transparent,
+                    borderRadius: BorderRadius.circular(20.0)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Center(
+                      child: Image.asset(
+                        'assets/facebook.png',
+                        height: 25.0,
+                      ),
+                    ),
+                    SizedBox(width: 10.0),
+                    Center(
+                      child: Text(
+                        'Log in with facebook',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
                         ),
-                        color: Colors.transparent,
-                        borderRadius: BorderRadius.circular(20.0)),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Center(
-                          child: Image.asset(
-                            'assets/facebook.png',
-                            height: 25.0,
-                          ),
-                        ),
-                        SizedBox(width: 10.0),
-                        Center(
-                          child: Text(
-                            'Log in with facebook',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        )
-                      ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('New to Flutter ?'),
+                SizedBox(width: 5.0),
+                InkWell(
+                  onTap: () {
+                    Navigator.of(context).pushNamed('/signup');
+                  },
+                  child: Text(
+                    'Sign in',
+                    style: TextStyle(
+                      color: Colors.blueAccent[400],
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
                 )
               ],
-            ),
-          ),
-          SizedBox(height: 20.0),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text('New to Flutter ?'),
-              SizedBox(width: 5.0),
-              InkWell(
-                onTap: () {
-                  Navigator.of(context).pushNamed('/signup');
-                },
-                child: Text(
-                  'Sign in',
-                  style: TextStyle(
-                    color: Colors.blueAccent[400],
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              )
-            ],
-          )
-        ],
-      ),
-    );
+            )
+          ],
+        ));
   }
 }
